b89a7bb16105a1002971c28cf42dc1dabf7cecbf
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2020-05-07 21:35:27 +0200

    HAL_CAN_RxFifo0MsgPendingCallback() interrupt handler added

--- src/interrupts.cpp
+++ src/interrupts.cpp
@@ -4,6 +4,7 @@
 
 extern void spi_SensorTxCpltCallback();
 extern void spi_SensorTxRxCpltCallback();
+extern void micro_Vehicle_Can_RxFifoMsgPendingCallback();
 
 extern "C" void HAL_SPI_TxCpltCallback(SPI_HandleTypeDef *hspi) {
     if (hspi == spi_Sensor) {
@@ -14,5 +15,11 @@ extern "C" void HAL_SPI_TxCpltCallback(SPI_HandleTypeDef *hspi) {
 extern "C" void HAL_SPI_TxRxCpltCallback(SPI_HandleTypeDef *hspi) {
     if (hspi == spi_Sensor) {
         spi_SensorTxRxCpltCallback();
+    }
+}
+
+extern "C" void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan) {
+    if (hcan == can_Vehicle) {
+        micro_Vehicle_Can_RxFifoMsgPendingCallback();
     }
 }
