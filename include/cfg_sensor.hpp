53aacbb38f5f33b37eaea078d68623698bf8ed20
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2020-10-14 21:27:54 +0200

    line probability configs updated

--- include/cfg_sensor.hpp
+++ include/cfg_sensor.hpp
@@ -11,8 +11,8 @@ constexpr micro::millimeter_t MAX_LINE_JUMP          = micro::millimeter_t(8);
 constexpr micro::millimeter_t MIN_LINE_DIST          = micro::millimeter_t(25);
 constexpr int8_t LINE_FILTER_HYSTERESIS              = 10;
 constexpr uint8_t LINE_FILTER_VELO_PEEK_BACK         = 5;
-constexpr float MIN_LINE_PROBABILITY                 = 0.25f;
-constexpr float MIN_LINE_APPEAR_PROBABILITY          = 0.4f;
+constexpr float MIN_LINE_PROBABILITY                 = 0.4f;
+constexpr float MIN_LINE_APPEAR_PROBABILITY          = 0.6f;
 constexpr micro::millimeter_t OPTO_ARRAY_LENGTH      = micro::millimeter_t(274.574f);
 
 } // namespace cfg
