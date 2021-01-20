87c4d6ffa376960d50d0ad0bac1fcf41e9f5d604
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2021-01-20 17:38:45 +0100

    line position calculation configs updated

--- include/cfg_sensor.hpp
+++ include/cfg_sensor.hpp
@@ -6,15 +6,15 @@ namespace cfg {
 
 constexpr uint8_t MAX_NUM_FILTERED_LINES             = 6;
 constexpr uint8_t NUM_SENSORS                        = 48;
-constexpr uint8_t LINE_POS_CALC_OFFSET_FILTER_RADIUS = 8;
+constexpr uint8_t LINE_POS_CALC_OFFSET_FILTER_RADIUS = 3;
 constexpr float LINE_POS_CALC_INTENSITY_GROUP_RADIUS = 0.5f;
 constexpr float LINE_POS_CALC_GROUP_RADIUS           = 1.0f;
-constexpr micro::millimeter_t MAX_LINE_JUMP          = micro::millimeter_t(8);
-constexpr micro::millimeter_t MIN_LINE_DIST          = micro::millimeter_t(15);
-constexpr int8_t LINE_FILTER_HYSTERESIS              = 5;
+constexpr micro::millimeter_t MAX_LINE_JUMP          = micro::millimeter_t(20);
+constexpr micro::millimeter_t MIN_LINE_DIST          = micro::millimeter_t(25);
+constexpr int8_t LINE_FILTER_HYSTERESIS              = 7;
 constexpr uint8_t LINE_VELO_FILTER_SIZE              = 4;
-constexpr uint8_t LINE_POS_FILTER_WINDOW_SIZE        = 8;
-constexpr float MIN_LINE_PROBABILITY                 = 0.65f;
+constexpr uint8_t LINE_POS_FILTER_WINDOW_SIZE        = 1;
+constexpr float MIN_LINE_PROBABILITY                 = 0.45f;
 constexpr micro::millimeter_t OPTO_ARRAY_LENGTH      = micro::millimeter_t(274.574f);
 
 } // namespace cfg
