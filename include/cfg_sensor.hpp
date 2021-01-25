47c1957c9522f8bcb1a3d2645aa1a4b2a9cd75f0
Soma Veszelovszki <soma.veszelovszki@gmail.com>
2021-01-25 21:27:38 +0100

    more leniant line detection config

--- include/cfg_sensor.hpp
+++ include/cfg_sensor.hpp
@@ -12,10 +12,10 @@ constexpr float LINE_POS_CALC_INTENSITY_GROUP_RADIUS = 0.5f;
 constexpr float LINE_POS_CALC_GROUP_RADIUS           = 1.0f;
 constexpr micro::millimeter_t MAX_LINE_JUMP          = micro::millimeter_t(20);
 constexpr micro::millimeter_t MIN_LINE_DIST          = micro::millimeter_t(25);
-constexpr int8_t LINE_FILTER_HYSTERESIS              = 7;
+constexpr int8_t LINE_FILTER_HYSTERESIS              = 4;
 constexpr uint8_t LINE_VELO_FILTER_SIZE              = 4;
 constexpr uint8_t LINE_POS_FILTER_WINDOW_SIZE        = 1;
-constexpr float MIN_LINE_PROBABILITY                 = 0.45f;
+constexpr float MIN_LINE_PROBABILITY                 = 0.40f;
 constexpr micro::millimeter_t OPTO_ARRAY_LENGTH      = micro::millimeter_t(274.574f);
 
 } // namespace cfg
